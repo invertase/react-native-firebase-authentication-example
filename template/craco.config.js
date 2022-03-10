68bd37f8c219f892655b40c73e9818370f368b82
Mike Hardy <github@mikehardy.net>
2022-03-09 20:06:14 -0500

    fix(web): silence warnings during web prod build

--- template/craco.config.js
+++ template/craco.config.js
@@ -87,6 +87,8 @@ module.exports = {
           runtime: 'automatic'
         }
       ],
+      ['@babel/plugin-proposal-decorators', {legacy: true}],
+      ['@babel/plugin-proposal-class-properties', {loose: true}],
       ["@babel/plugin-proposal-private-methods", { "loose": true }],
       ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
     ],
